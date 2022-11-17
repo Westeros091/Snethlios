e04c55dc9e9ccff7219fac2a033b70a578b527cf
Hieu <westeros091@gmail.com>
2022-11-17 21:06:42 +0700

    Hieu: update Java

---
+++ ui_test1/src/com/snethlios/dao/PhieuXuatDAO.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.snethlios.dao;
+
+import com.snethlios.entity.PhieuXuat;
+import com.snethlios.utils.JdbcHelper;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class PhieuXuatDAO extends SnethliosDAO<PhieuXuat, Integer>{
+
+    @Override
+    public void insert(PhieuXuat entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(PhieuXuat entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Integer key) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<PhieuXuat> selectAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public PhieuXuat selectByID(Integer key) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    protected List<PhieuXuat> selectBySQL(String sql, Object... args) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    public List<Integer> selectYears() {
+        String sql = "SELECT DISTINCT year(NgayTao) Year FROM PhieuXuat ORDER BY Year DESC";
+        List<Integer> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.query(sql);
+            while (rs.next()) {
+                list.add(rs.getInt(1));
+            }
+            rs.getStatement().getConnection().close();
+            return list;
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+}
